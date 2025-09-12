e16198999434d9ec8a020c32772853d2e9c28f77
DHURSHITHA <dhurshitiha.n2006@gmail.com>
2025-09-13 00:42:42 +0530

    Ui updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      { source: "/dashboard/faculty_dashboard", destination: "/dashboard/faculty-dashboard" },
+    ];
+  },
 };
 
 export default nextConfig;
